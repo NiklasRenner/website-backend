05b73d32a0ad137854f5870cbafe6b73c8dcf5d6
Niklas Renner Nielsen <NiklasRennerNielsen@gmail.com>
2019-03-02 16:13:27 +0100

    made backend deployable to tomcat.

--- src/main/kotlin/id/renner/backend/BackendApplication.kt
+++ src/main/kotlin/id/renner/backend/BackendApplication.kt
@@ -2,9 +2,10 @@ package id.renner.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class BackendApplication
+class BackendApplication : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
 	runApplication<BackendApplication>(*args)
