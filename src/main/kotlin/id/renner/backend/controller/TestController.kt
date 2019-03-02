098ede9b302a83de862bd730e3030a586c36b7a8
Niklas Renner Nielsen <NiklasRennerNielsen@gmail.com>
2019-03-02 19:59:57 +0100

    commit to test that continuous deployment works

--- src/main/kotlin/id/renner/backend/controller/TestController.kt
+++ src/main/kotlin/id/renner/backend/controller/TestController.kt
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.RestController
 class TestController {
     private val services = listOf(
             ServiceTestDto(1, "test-service", "127.0.0.1", "A service for testing."),
-            ServiceTestDto(1, "user-service", "255.255.255.255", "Contains user data."),
-            ServiceTestDto(1, "some-other-service", "192.168.1.1", "Does stuff(?)."),
-            ServiceTestDto(1, "old-service", "0.0.0.0", "At this point nobody knows what this does."),
-            ServiceTestDto(1, "test-service-2", "127.127.127.127", "Can never have enough tests")
+            ServiceTestDto(2, "user-service", "255.255.255.255", "Contains user data."),
+            ServiceTestDto(3, "some-other-service", "192.168.1.1", "Does stuff(?)."),
+            ServiceTestDto(4, "old-service", "0.0.0.0", "At this point nobody knows what this does."),
+            ServiceTestDto(5, "test-service-2", "127.127.127.127", "Can never have enough tests"),
+            ServiceTestDto(6, "cdeploy-service", "13.37.13.37", "Wait this is in prod?")
     )
 
     @GetMapping("/services")
