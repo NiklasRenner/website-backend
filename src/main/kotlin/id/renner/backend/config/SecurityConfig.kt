6656c1603ec7171ada83dd9a75792018145b614c
Niklas Renner Nielsen <NiklasRennerNielsen@gmail.com>
2019-03-04 02:16:36 +0100

    updated security config a bit for proper http response code etc.

--- src/main/kotlin/id/renner/backend/config/SecurityConfig.kt
+++ src/main/kotlin/id/renner/backend/config/SecurityConfig.kt
@@ -2,24 +2,33 @@ package id.renner.backend.config
 
 import id.renner.backend.config.filter.JwtAuthenticationFilter
 import id.renner.backend.config.filter.JwtAuthorizationFilter
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import javax.servlet.Filter
 
 
 @Configuration
-class SecurityConfig(val fakeUserDetailsService: UserDetailsService, val passwordEncoder: PasswordEncoder) : WebSecurityConfigurerAdapter() {
+class SecurityConfig(val fakeUserDetailsService: UserDetailsService) : WebSecurityConfigurerAdapter() {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
         http.httpBasic().disable()
                 .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
@@ -30,11 +39,13 @@ class SecurityConfig(val fakeUserDetailsService: UserDetailsService, val passwor
                 .logout().disable()
                 .addFilterBefore<UsernamePasswordAuthenticationFilter>(JwtAuthenticationFilter(authenticationManager(), "/login"))
                 .addFilterBefore<UsernamePasswordAuthenticationFilter>(JwtAuthorizationFilter(authenticationManager()))
+                .exceptionHandling()
+                .authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
     }
 
     @Throws(Exception::class)
     override fun configure(managerBuilder: AuthenticationManagerBuilder) {
-        managerBuilder.userDetailsService(fakeUserDetailsService).passwordEncoder(passwordEncoder)
+        managerBuilder.userDetailsService(fakeUserDetailsService).passwordEncoder(passwordEncoder())
     }
 }
 
