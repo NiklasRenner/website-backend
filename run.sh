e1f4f5371220ef74db50e9970def4687059a4a57
NiklasRenner <NiklasRennerNielsen@gmail.com>
2019-07-21 00:21:38 +0200

    trying detached mode with health poll.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export GENERATED_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-docker-compose up --force-recreate --build
+docker-compose up -d --force-recreate --build
+timeout 30 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' https://dev.renner.id/ip)" != "200" ]]; do sleep 5; done' || false
