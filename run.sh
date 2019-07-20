a63a53c95f1cc1c3a37dd4b719043a1db54cb6c7
NiklasRenner <NiklasRennerNielsen@gmail.com>
2019-07-21 00:43:53 +0200

    spitting out some info to check that everything works as expected.

--- run.sh
+++ run.sh
@@ -2,5 +2,9 @@
 
 export GENERATED_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 docker-compose up -d --force-recreate --build
-timeout 30 bash -c 'while [[ "$(ip_result = curl -s https://dev.renner.id/ip)" != "" ]]; do sleep 5; done' || false
-echo $ip_result
+
+timeout 30 bash -c 'while [[ "$(curl -s https://dev.renner.id/ip)" != "" ]]; do sleep 5; done' || false
+
+echo $GENERATED_KEY
+echo done
+curl -s https://dev.renner.id/ip
